dfacea8308b90800614114df23fe40730d6ab2fe
Ivan Tkachuk <ivktac@gmail.com>
2024-10-02 13:24:10 +0300

    Finish react course 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,14 @@
-function Navbar() {}
+import logo from "../images/logo.svg";
+import styles from "./Navbar.module.css";
+
+function Navbar() {
+  return (
+    <nav className={styles.nav}>
+      <img src={logo} alt="logo" className={styles.navLogo} />
+      <h3 className={styles.navLogoText}>ReactFacts</h3>
+      <h4 className={styles.navTitle}>React Course - Project 1</h4>
+    </nav>
+  );
+}
 
 export default Navbar;

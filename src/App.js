2979326a19dfa3da92a636d8bd80d1b31dce92e1
Ivan Tkachuk <ivktac@gmail.com>
2024-10-02 12:57:32 +0300

    Clean code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <Footer />
+      <Navbar />
+      <Main />
     </div>
   );
 }
--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer>
-      <small>© 2024 Ivan Tkachuk.All rights reserved.</small>
-    </footer>
-  );
-}
-
-export default Footer;
